5ebd297a2927a7dd9e57ea2d5b2c1c96271c9a15
Tongeghanita <ghanitatonge97@gmail.com>
2026-03-23 14:44:05 +0530

    Add files via upload

---
+++ inhertance.py
@@ -0,0 +1,27 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+class Employee(Person):
+    def __init__(self, name, age, emp_id):
+        super().__init__(name, age)
+        self.emp_id = emp_id
+
+
+class Manager(Employee):
+    def __init__(self, name, age, emp_id, department):
+        super().__init__(name, age, emp_id)
+        self.department = department
+
+    def display(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+        print("ID:", self.emp_id)
+        print("Department:", self.department)
+
+
+# Object
+m = Manager("Radha", 21, 101, "IT")
+m.display()
