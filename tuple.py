8976d58f0e2059fdb5693c35ff373614263179fe
Tongeghanita <ghanitatonge97@gmail.com>
2026-03-23 14:43:13 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,13 @@
+# Create tuple
+t = (1, 2, 3)
+
+# Access
+print("First element:", t[0])
+
+# Nested tuple
+nested = (1, (2, 3), 4)
+print("Nested tuple:", nested)
+
+# Repetition
+repeat = t * 2
+print("Repeated tuple:", repeat)
