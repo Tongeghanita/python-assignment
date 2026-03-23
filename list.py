b6045b1ec3c7b3c44c9c197b407e05841572472c
Tongeghanita <ghanitatonge97@gmail.com>
2026-03-23 14:42:54 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,17 @@
+# Create list
+my_list = [10, 20, 30, 40]
+
+# Access elements
+print("First element:", my_list[0])
+
+# Add element
+my_list.append(50)
+print("After adding:", my_list)
+
+# Remove element
+my_list.remove(20)
+print("After removing:", my_list)
+
+# Sort list
+my_list.sort()
+print("Sorted list:", my_list)
