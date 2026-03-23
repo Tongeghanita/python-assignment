54452b52b76c047375b03c5f61586c2a0619e289
Tongeghanita <ghanitatonge97@gmail.com>
2026-03-23 14:43:30 +0530

    Add files via upload

---
+++ dic.py
@@ -0,0 +1,13 @@
+# Create dictionary
+student = {"name": "Radha", "age": 18}
+
+# Access
+print("Name:", student["name"])
+
+# Update
+student["age"] = 19
+print("Updated:", student)
+
+# Remove element
+del student["age"]
+print("After deletion:", student)
